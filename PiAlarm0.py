ab038292c562ff62cd8c1fd0d773a75cf31af175
Sush <sushanta1996@gmail.com>
2017-05-30 20:44:19 +1000

    Update PiAlarm0.py

--- PiAlarm0.py
+++ PiAlarm0.py
@@ -17,40 +17,40 @@ def main():
  
 def alarmTime():
     t1 = '07:30:00'
-    t2 = '17:23:50'
+    t2 = '13:18:30'
     return t2
 	
+def tones():
+    url0 ="https://www.pandora.com/stations/"
+    
+    return url0;
 	
 def runAlarm():
-    def alarm():
-        
-        
-        threading.Timer(1,alarm).start()
-        timenow = datetime.now()       
-        al1 = str(timenow.strftime("%H:%M:%S"))
-        print(al1)
+
+    timenow = datetime.now()       
+    al1 = str(timenow.strftime("%H:%M:%S"))
+    print(al1)
+       
+    if al1== alarmTime():    
+
         x = randint(1,6)
-        if al1== alarmTime():           
-            #webbrowser.open(tones(), new=0)
-            x = randint(1,6)
-            mixer.init()
-            mixer.music.load('path/to/song%d.mp3'% x)
-            mixer.music.play()            
-            print('ALARM! ALARM!')    
-            sleep(180)           
-            mixer.music.fadeout(7000)
-            sleep(7) 
-            mixer.quit()
-            print('Mixer Ended')
+        mixer.init()
+        mixer.music.load('C:/Users/New/Dropbox/Apps/New folder/song%d.mp3'% x)
+        mixer.music.play()            
+        print('ALARM! ALARM!')    
+        sleep(180)           
+        mixer.music.fadeout(7000)
+        sleep(7) 
+        mixer.quit()
+        print('Mixer Ended')
             
             
-        if al1 != alarmTime():
-             print("Systems Normal")
-             
-
-    alarm()
-
- 
+    if al1 != alarmTime():
+        print("Systems Normal")
+    sleep(1)
+    runAlarm()
+     
+     
 
 	
 if __name__ == "__main__":
