16b86cd4bb7b5d85b1520259c720204bd6a5ceb9
git996 <iamnewdev@gmail.com>
2017-06-24 13:56:32 +1000

    re-instating  threading
    going back to using threading due to recursion limits

--- PiAlarm0.py
+++ PiAlarm0.py
@@ -5,51 +5,49 @@ from datetime import time
 from datetime import datetime
 from datetime import timedelta
 import threading
+import sys
 from time import time,sleep
 from pygame import mixer
 from random import *
 
 
 def main():
-   
    runAlarm()
-   
- 
+
+
 def alarmTime():
-    t1 = '07:30:00'
-    t2 = '16:38:30'
-    return t2
-	
-	
+    t1 = '07:00:00'
+    t2 = '13:20:10'
+    return t1
+
+
 def runAlarm():
+    def alarm():
+        threading.Timer(1,alarm).start()
+        timenow = datetime.now()
+        al1 = str(timenow.strftime("%H:%M:%S"))
+        print(al1)
+        x = randint(1,119)
+        if al1== alarmTime():
+            #webbrowser.open(tones(), new=0)
 
-    timenow = datetime.now()       
-    al1 = str(timenow.strftime("%H:%M:%S"))
-    print(al1)
-       
-    if al1== alarmTime():    
+            mixer.init()
+            mixer.music.load('C:/Users/New/Music/im/iMusic/song (%d).mp3'% x)
+            mixer.music.play()
+            print('ALARM!')
+            sleep(240)
+            mixer.music.fadeout(7000)
+            sleep(7)
+            mixer.quit()
+            print('Closing Mixer')
 
-        x = randint(1,119) 
-        
-        mixer.init()
-        mixer.music.load('C:/Users/New/Music/im/iMusic/song (%d).mp3'% x)
-        mixer.music.play()            
-        print('ALARM! ALARM!')    
-        sleep(180)           
-        mixer.music.fadeout(7000)
-        sleep(7) 
-        mixer.quit()
-        print('Mixer Ended')
-            
-            
-    if al1 != alarmTime():
-        print("Systems Normal")
-    sleep(1)
-    runAlarm()
-     
-     
+        if al1 != alarmTime():
+             print("Systems Normal! Alarm set for: ", alarmTime())
 
-	
+    alarm()
+
+
+
 if __name__ == "__main__":
     # execute only if run as a script
-    main() 
+    main()
