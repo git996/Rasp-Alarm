a24f4e5d2a5d5a9b2cf0433fba72af1386aa32f2
git996 <iamnewdev@gmail.com>
2017-07-04 01:13:37 +1000

    PyAlarm0.pi

--- PiAlarm0.py
+++ PiAlarm0.py
@@ -9,40 +9,35 @@ import sys
 from time import time,sleep
 from pygame import mixer
 from random import *
+import schedule
 
 
 def main():
-   runAlarm()
+   schedule.every().day.at("19:01").do(runAlarm)
+   while True:
+      schedule.run_pending()
+      sleep(1)
+   
 
 
-def alarmTime():
-    t1 = '07:00:00'
-    t2 = '13:20:10'
-    return t1
+
 
 
 def runAlarm():
     def alarm():
-        threading.Timer(1,alarm).start()
-        timenow = datetime.now()
-        al1 = str(timenow.strftime("%H:%M:%S"))
-        print(al1)
         x = randint(1,119)
-        if al1== alarmTime():
-            #webbrowser.open(tones(), new=0)
 
-            mixer.init()
-            mixer.music.load('C:/Users/New/Music/im/iMusic/song (%d).mp3'% x)
-            mixer.music.play()
-            print('ALARM!')
-            sleep(240)
-            mixer.music.fadeout(7000)
-            sleep(7)
-            mixer.quit()
-            print('Closing Mixer')
+        mixer.init()
+        mixer.music.load('C:/Users/New/Music/im/iMusic/song (%d).mp3'% x)
+        mixer.music.play()
+        print('ALARM!')
+        sleep(240)
+        mixer.music.fadeout(7000)
+        sleep(7)
+        mixer.quit()
+        print('Closing Mixer')
 
-        if al1 != alarmTime():
-             print("Systems Normal! Alarm set for: ", alarmTime())
+      
 
     alarm()
 
